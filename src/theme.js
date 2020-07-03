e6c08db8fe5a06e65a242469cba362fbe907fb1a
Abhinav Sachdeva <happy257@gmail.com>
2020-07-03 14:12:58 +0530

    theme

--- src/theme.js
+++ src/theme.js
@@ -1,14 +1,33 @@
 import { createMuiTheme } from "@material-ui/core";
+// const darkTheme = {
+// 	palette: {
+// 		type: 'dark',
+// 		primary: { main: '#07ddff' },
+// 		secondary: { main: '#04aeca' },
+// 		background: {
+// 			paper: '#424242'
+// 		}
+// 	},
+// 	typography: {
+// 		fontFamily: 'Nunito, sans-serif',
+// 	}
+// };
 
-const theme = createMuiTheme({
+const lightTheme = {
 	palette: {
-		primary: { main: "#1d4354" }
+		primary: {
+			main: "#1d4354",
+		},
+		secondary: {
+			main: "#efefef"
+		},
+		type: "light"
 	},
 	typography: {
 		fontFamily: 'Nunito, sans-serif',
-	},
-	overrides: {
-	},
-});
+	}
+}
+
+const theme = createMuiTheme(lightTheme);
 
 export default theme
